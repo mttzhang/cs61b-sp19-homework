eb8fcb66eef69b304e1b101161a7b13d01291141
Mengting Zhang <mtzhang2212@gmail.com>
2020-06-30 21:17:35 -0400

    first commit

---
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,148 @@
+package es.datastructur.synthesizer;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayRingBuffer<T> implements BoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;
+    /* Index for the next enqueue. */
+    private int last;
+    /* Variable for the fillCount. */
+    private int fillCount;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    private int wizP;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        wizP = -1;
+    }
+
+    @Override
+    public int capacity() {
+        return rb.length;
+    }
+
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow").
+     */
+    @Override
+    public void enqueue(T x) {
+
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        fillCount++;
+        last++;
+        last = Math.floorMod(last, capacity());
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow").
+     */
+    public T dequeue() {
+
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T result = rb[first];
+        first++;
+        first = Math.floorMod(first, capacity());
+        fillCount--;
+        return result;
+    }
+
+    /**
+     * Return oldest item, but don't remove it. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow").
+     */
+    public T peek() {
+
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    private class BufferIterator implements Iterator<T> {
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return (wizP - first) < fillCount();
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public T next() {
+            if (wizP == -1) {
+                wizP = first;
+            }
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T results = rb[Math.floorMod(wizP, capacity())];
+            wizP++;
+            return results;
+        }
+    }
+
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (this == o) {
+            return true;
+        }
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+        ArrayRingBuffer<T> other = (ArrayRingBuffer<T>) o;
+        if (this.fillCount != other.fillCount) {
+            return false;
+        }
+        int index = 0;
+        for (T item: other) {
+            if (!rb[index].equals(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
