89a399c76acdcfc29791f98309f3fc26d5b3a4b9
Mengting Zhang <mtzhang2212@gmail.com>
2020-07-03 19:52:46 -0400

    first commit

---
+++ hw2/hw2/Percolation.java
@@ -0,0 +1,114 @@
+package hw2;
+
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    // create N-by-N grid, with all sites initially blocked
+    int n;
+    WeightedQuickUnionUF grid;
+    boolean[] isOpen;
+    int upDotIndex;
+    int downDotIndex;
+    int size;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        n = N;
+        size = 0;
+
+        grid = new WeightedQuickUnionUF(N * N + 2);
+        upDotIndex = N * N;
+        downDotIndex = upDotIndex + 1;
+
+        isOpen = new boolean[N * N];
+        for (int i = 0; i < isOpen.length; i++) {
+            this.isOpen[i] = false;
+        }
+
+
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col)   {
+        if (!(0 <= row && row < n) || !(0 <= col && col < n)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        int index = xyTo1D(row, col);
+        isOpen[index] = true;
+        size++;
+        if (index < n) {
+            grid.union(index, upDotIndex);
+        }
+        if ((n - 1) * n <= index) {
+            grid.union(index, downDotIndex);
+        }
+
+        int[] neighbor = findNeighborIndex(row, col);
+        for (int i : neighbor) {
+            if (i > -1 && isOpen(i)) {
+                grid.union(index, i);
+            }
+        }
+    }
+
+    //return the index
+    private int xyTo1D(int r, int c) {
+        return r * n + c;
+    }
+
+    private int[] findNeighborIndex(int r, int c) {
+        int i = xyTo1D(r, c);
+        int [] neighbor  = {-1, -1, -1, -1};
+        if (n <= i) {
+            neighbor[0] = i - n;
+        }
+        if (i < n * (n - 1)) {
+            neighbor[1] = i + n;
+        }
+        if (c != 0) {
+            neighbor[2] = i - 1;
+        }
+        if (c != (n - 1)) {
+            neighbor[3] = i + 1;
+        }
+        return neighbor;
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        int index = xyTo1D(row, col);
+        return isOpen[index];
+    }
+
+    private boolean isOpen(int index) {
+        return isOpen[index];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        int index = xyTo1D(row, col);
+        boolean isFull = grid.connected(index, upDotIndex);
+
+        return isFull;
+    }
+
+    // number of open sites, constant time
+    public int numberOfOpenSites() {
+        return size;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return grid.connected(upDotIndex, downDotIndex);
+    }
+
+}
---
+++ hw2/hw2/PercolationStats.java
@@ -0,0 +1,59 @@
+package hw2;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class PercolationStats {
+
+    double[] thresholds;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        thresholds = new double[T];
+        for (int t = 0; t < T; t++) {
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                p.open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+            double threshold = p.size / N * N;
+            thresholds[t] = threshold;
+        }
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        double sum = 0;
+        for (double threshold : thresholds) {
+            sum += threshold;
+        }
+        return sum / thresholds.length;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        double mean = mean();
+
+        double sum = 0;
+
+        for (double threshold : thresholds) {
+            sum += Math.pow((threshold - mean), 2);
+        }
+        return sum / (thresholds.length - 1);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        double mean = mean();
+        double sigma = stddev();
+        return mean - (1.96 * sigma) / Math.sqrt(thresholds.length);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + (1.96 * stddev()) / Math.sqrt(thresholds.length);
+    }
+
+}
